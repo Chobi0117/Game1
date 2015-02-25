048bb7090f727a12ff0ba4a42413520e0035f8ea
Kyosuke Nakagawa <kyosuke1248@yahoo.co.jp>
2015-02-25 22:55:27 +0900

    2DGameStart

---
+++ TestProject/Assets/script/chara.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+var posx : float;
+var posy : float;
+
+function Start () {
+
+	posx = -50;
+	posy = 0;
+
+}
+
+function Update () {
+	if(Input.GetKey( KeyCode.RightArrow))
+	{
+		posx = posx+0.2;
+	}
+	
+	if(Input.GetKey(KeyCode.LeftArrow))
+	{
+		posx = posx-0.2;
+	}
+	
+	if(Input.GetKeyDown(KeyCode.Space))
+	{
+		rigidbody2D.AddForce(Vector2(0,1300));
+	}
+	transform.position.x = posx;
+	transform.position.y += posy;
+	posy = 0;
+}
