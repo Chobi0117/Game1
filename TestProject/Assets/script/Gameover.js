bac20d971c3c7d057b71e7d4c75d3b43e1a92278
Kyosuke Nakagawa <kyosuke1248@yahoo.co.jp>
2015-02-28 00:21:52 +0900

    gameover

--- TestProject/Assets/script/Gameover.js
+++ TestProject/Assets/script/Gameover.js
@@ -1,8 +1,11 @@
 ﻿#pragma strict
 
+var guiskin : GUISkin;
+
 function OnGUI () {
-	if(GUI.Button(Rect(Screen.width/2-100,200,
-		200,30),"Title"))
+	GUI.skin = guiskin;
+	GUI.TextField(Rect(Screen.width/2-100,200,200,30),"GAME OVER", "textfield");
+	if(GUI.Button(Rect(Screen.width/2-100,300,200,30),"Title"))
 	{
 		Application.LoadLevel("title");		
 	}
