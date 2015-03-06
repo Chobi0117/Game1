19ecbeed3a61de2e00f05ab23c77bfe857e0dddc
Kyosuke Nakagawa <kyosuke1248@yahoo.co.jp>
2015-03-06 22:11:08 +0900

    stage2finish

---
+++ TestProject/Assets/script/stage2move2.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+public var player : Transform;
+var posy : float;
+
+function Start () {
+
+}
+
+function Update () {
+	if(player.position.x > -10)
+	{
+		transform.position.y -= 0.8;
+	}
+}
