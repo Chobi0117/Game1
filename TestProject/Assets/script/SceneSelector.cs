0d8b840afc429e206c7f86cd9793c41b83e78f6e
Kyosuke Nakagawa <kyosuke1248@yahoo.co.jp>
2015-03-27 22:11:22 +0900

    NewTitle画面で選択した時に音をなるように

--- TestProject/Assets/script/SceneSelector.cs
+++ TestProject/Assets/script/SceneSelector.cs
@@ -3,17 +3,20 @@ using System.Collections;
 
 public class SceneSelector : MonoBehaviour
 {
+		private bool loadLevel = false;
 
 		// Use this for initialization
 		void Start ()
 		{
-
+			
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
-	
+				if (loadLevel && !gameObject.GetComponent<AudioSource> ().isPlaying) {
+						Application.LoadLevel (transform.name);
+				}
 		}
 
 		public void onTapButton ()
@@ -23,7 +26,9 @@ public class SceneSelector : MonoBehaviour
 
 		public void OnClick ()
 		{
-				Application.LoadLevel (transform.name);
+				loadLevel = true;
+//				Application.LoadLevel (transform.name);
+				gameObject.GetComponent<AudioSource> ().Play ();
 			
 		}
 }
