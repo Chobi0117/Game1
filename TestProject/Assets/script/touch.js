0b28ef407c048f54dbbab917b3a2e440fa92ad7a
Kyosuke Nakagawa <kyosuke1248@yahoo.co.jp>
2015-04-04 22:11:39 +0900

    touch

---
+++ TestProject/Assets/script/touch.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+private  var tapx: float;
+function Start () {
+
+}
+
+function Update () { 
+	for (var touch : Touch in Input.touches) {
+    	if (touch.phase == TouchPhase.Began) {
+  			tapx = touch.position.x;      
+        }
+   	    if (touch.phase == TouchPhase.Moved||touch.phase == TouchPhase.Stationary) {
+   	    	Debug.Log(touch.position.x);
+    		if(touch.position.x-tapx>1){
+    			transform.position.x += 0.1;
+    		}
+    		if(touch.position.x-tapx<1){
+    			transform.position.x -= 0.1;
+    		}
+    	}
+    }
+}
